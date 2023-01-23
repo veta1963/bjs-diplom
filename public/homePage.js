bd483501dbc4fc903a082f5c86bf9bc928b4f6f7
veta1963 <seagull-v@bk.ru>
2023-01-23 19:30:44 +0500

    homePage

--- public/homePage.js
+++ public/homePage.js
@@ -1,8 +1,14 @@
-class LogoutButton {
-    constructor () {
-        this.logoutButton = new LogoutButton();
-    }
-    action = function() {
+'use strict'
+
+const logoutButton = new LogoutButton();
+
+logoutButton.action();
+
+ApiConnector.logout(response => console.log(response));
+
+{success};
+
+location.reload();
+    
+
         
-    }
-}
