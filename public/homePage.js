5c0ba6250c34deaee0a4d76d2c351136108390b5
veta1963 <104830918+veta1963@users.noreply.github.com>
2023-04-15 19:55:32 +0500

    Update homePage.js

--- public/homePage.js
+++ public/homePage.js
@@ -1,14 +1,104 @@
-'use strict'
 
-const logoutButton = new LogoutButton();
 
-logoutButton.action();
+const logout = new LogoutButton();
+logout.action = function() {
+    ApiConnector.logout(function(response){
+        if(response.success){
+            location.reload();
+        }
+    });
+}
 
-ApiConnector.logout(response => console.log(response));
+ApiConnector.current(function(response){
+    if(response.success){
+        ProfileWidget.showProfile(response.data);
+    }
+});
 
-{success};
+const rates = new RatesBoard();
+function exchangeRate() {
+    ApiConnector.getStocks(function(response){
+        if(response.success){
+            rates.clearTable();
+            rates.fillTable(response.data);
+        }
+    });
+}
+function exchangeRatePerMunute(){
+    setInterval(exchangeRate(), 60000);
+}
+exchangeRatePerMunute();
 
-location.reload();
+const money = new MoneyManager();
+money.addMoneyCallback = function(data) {
+    ApiConnector.addMoney(data, (response) =>{
+        if(response.success){
+            ProfileWidget.showProfile(response.data);
+            money.setMessage(false, `Успешное пополонение баланса`);
+        } else {
+            money.setMessage(true, response.data);
+        }
+    });
+}
+
+money.conversionMoneyCallback = function(data) {
+    ApiConnector.convertMoney(data, (response) =>{
+            if(response.success){
+                ProfileWidget.showProfile(response.data);
+                money.setMessage(false, `Успешная конвертация`);
+            } else {
+                money.setMessage(true, response.data);
+            }
+    });
+}
+
+money.sendMoneyCallback = function(data) {
+    ApiConnector.transferMoney(data, (response) =>{
+        console.log(response);
+            if(response.success){
+                ProfileWidget.showProfile(response.data);
+                money.setMessage(false, `Успешный перевод`);
+            } else {
+                money.setMessage(true, response.data);
+            }
+    });
+}
+
+const favorite = new FavoritesWidget();
+ApiConnector.getFavorites(function(response){
+    if(response.success){
+        console.log(response);
+        favorite.clearTable();
+        favorite.fillTable(response.data);
+        money.updateUsersList(response.data);
+    }
+});
+
+favorite.addUserCallback = function(data) {
+    ApiConnector.addUserToFavorites(data, (response) =>{
+        if (response.success){
+            favorite.clearTable();
+            favorite.fillTable(response.data);
+            money.updateUsersList(response.data);
+            money.setMessage(false, `Успешное добавление`);
+        } else {
+            money.setMessage(true, response.data);
+        }
+    });
+}
+
+favorite.removeUserCallback = function(data) {
+    ApiConnector.removeUserFromFavorites(data, (response) =>{
+        if (response.success){
+            favorite.clearTable();
+            favorite.fillTable(response.data);
+            money.updateUsersList(response.data);
+            favorite.setMessage(false, `Успешное удаление`);
+        } else {
+            favorite.setMessage(true, response.data);
+        }
+    });
+}
     
 
         
